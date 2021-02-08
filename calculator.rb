74f0cdd3bd6f964283bc1fdfa7152d93457ea10a
Zecharias Mengis <zmengis@gmail.com>
2021-02-08 17:49:21 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,6 @@
-# Add your variables here
+first_number = 3
+second_number = 5
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
